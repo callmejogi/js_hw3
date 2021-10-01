d21cf6640f85258dfa4eb66558fbc2b734229ce2
Heorhii <kutafin01@gmail.com>
2021-10-01 13:47:06 +0300

    completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let name = "Джон";
+let admin = `${name}`;
+alert(admin);
+
+const planetEarth = "Earth";
+const currentUser = prompt('What`s your name?');
+
+let num = '10' + 10;
+console.log(typeof num)
+console.log(`The name of user is:${currentUser}`)
